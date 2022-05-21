ac55c60ed140bcf99cd90b201fa31c7940c73a75
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2022-05-22 00:43:48 +0200

    lets gooo ekipa

---
+++ js/fourthwebsite/fourthwebsite.js
@@ -0,0 +1,30 @@
+var date = new Date();
+var head = document.getElementById("head");
+var light = date.getHours() >= 8 && date.getHours() <= 20;
+
+if(light)
+    head.innerHTML += "<link rel='stylesheet' href='\\styles\\fourthwebsite\\lightmode.css' />";
+else
+    head.innerHTML += "<link rel='stylesheet' href='\\styles\\fourthwebsite\\darkmode.css' />";
+    
+function onFocus(label){
+    let l = document.getElementById(label);
+    if(light){
+        l.style.color = "#000000";//input border color
+        l.style.backgroundColor = "#53918d";//form container background
+    }
+    else{
+        l.style.color = "#5f8b86";//input border color
+        l.style.backgroundColor = "#5f5f5f";//form container background
+    }
+
+}
+
+function onBlur(input, label){
+    let l = document.getElementById(label);
+    let i = document.getElementById(input);
+    if(i.value.length == 0){
+        l.style.color = "transparent";
+        l.style.background = "transparent";
+    }
+}
