29cb9f36bbb6995542757cf9e29a06ad1e96cff0
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2022-05-22 06:33:18 +0200

    test

--- php/post.php
+++ php/post.php
@@ -6,10 +6,10 @@
             $title = $post["title"];
             echo 
             "<div class='photo-container'>
-                <figure>
-                    <img src='../img/fourthwebsite/$image' alt='gallery image' srcset='' width='240px' height='240px' />
-                    <figcaption>$title</figcaption>
-                </figure>
+            <figure>
+                <img src='../img/fourthwebsite/$image' alt='gallery image' srcset='' width='240px' height='240px' />
+                <figcaption>$title</figcaption>
+            </figure>
             </div>";
         }
     }
