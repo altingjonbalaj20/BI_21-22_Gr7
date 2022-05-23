e95b96c33575b613050b399966993df8cbe810f5
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2022-05-23 16:49:01 +0200

    added try and catch - 2 exceptions

--- content/actions/saveimage.php
+++ content/actions/saveimage.php
@@ -2,31 +2,40 @@
 $target_dir = "../../img/fourthwebsite/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+try {
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 // Check if image file is a actual image or fake image
-if(isset($_POST["Create"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    $uploadOk = 1;
-  } else {
-    $uploadOk = 0;
-  }
+if (isset($_POST["Create"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+    }
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  $uploadOk = 0;
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    die("Image saving problem, on image save.");
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-  } else {
-  }
+try {
+    if ($uploadOk == 0) {
+        die("Image saving problem, on image save.");
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        } else {
+        }
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
-?>
